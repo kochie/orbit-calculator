6192856a89c0d18224a02eae3077b937ce11198c
Robert Koch <robert@kochie.io>
2018-07-06 15:56:55 +1000

    Added a constant file

---
+++ src/common/constants.ts
@@ -0,0 +1,11 @@
+// Universal Constants
+export const G: number = 6.6_740_831 * (10 ** -11);
+
+// Mass Constants
+export const Mearth: number = 5.972 * (10 ** 24);
+export const Msun: number = 1.989 * (10 ** 30);
+export const Mmoon: number = 7.348 * (10 ** 22);
+
+// Distance Constants
+export const AU: number = 149_597_870_700;
+export const LY: number = 9_460_730_472_580_800;
